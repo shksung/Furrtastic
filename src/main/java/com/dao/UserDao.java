0a8539e9373158a445777d47e0d4b5c549f45833
Kevin Sung <s.h.kevin.sung@gmail.com>
2019-04-16 13:23:18 -0500

    config

--- src/main/java/com/dao/UserDao.java
+++ src/main/java/com/dao/UserDao.java
@@ -7,16 +7,12 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.model.User;
 import com.util.HibernateUtil;
 
 
 @Repository ("userDao")
-@Transactional
-@EnableTransactionManagement
 
 public class UserDao {
 
